02fc769d04535102e55c4e7f1fe8b273e4486aaf
Linus Norton <linusnorton@gmail.com>
2025-11-27 18:23:36 +0000

    vite port config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000;
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    port: 3000,
+    port,
   },
 });
