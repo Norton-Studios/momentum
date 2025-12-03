cc274aae54960855d9ff60261592210cafaee001
Linus Norton <linusnorton@gmail.com>
2025-12-03 07:35:26 +0000

    Add github projects (#317)

--- crons/data-sources/github/index.ts
+++ crons/data-sources/github/index.ts
@@ -1,6 +1,8 @@
 import { commitScript } from "./commit.js";
 import { contributorScript } from "./contributor.js";
+import { issueScript } from "./issue.js";
+import { projectScript } from "./project.js";
 import { pullRequestScript } from "./pull-request.js";
 import { repositoryScript } from "./repository.js";
 
-export const scripts = [repositoryScript, contributorScript, commitScript, pullRequestScript];
+export const scripts = [repositoryScript, contributorScript, commitScript, pullRequestScript, projectScript, issueScript];
