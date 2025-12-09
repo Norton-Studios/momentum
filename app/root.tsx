28e81149dce9a4dea62cc3a9a0a224c80cdd8c40
Linus Norton <linusnorton@gmail.com>
2025-12-09 18:20:41 +0000

    update logo

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import type { Route } from "./+types/root";
 import "./styles/global.css";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
